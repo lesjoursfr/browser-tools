2fd78a8f01a55e213352e754b54089e40fa255b7
Adrien ERAUD <e-adrien@users.noreply.github.com>
2024-02-01 10:44:35 +0100

    Fix Github Actions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,5 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 };
